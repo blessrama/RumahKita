5a67dea687297b08d992d58784a218cfb72de8e1
blessrama <bramasuteja@gmail.com>
2018-04-29 14:29:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/faturrahman/rumahkita/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.faturrahman.rumahkita;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.faturrahman.rumahkita.Tab.MyAdapter;
+import com.example.faturrahman.rumahkita.Tab.SlidingTabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private SlidingTabLayout mSlidingTabLayout;
+    private ViewPager mViewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mViewPager=(ViewPager)findViewById(R.id.st1_tabs);
+        mViewPager.setAdapter(new MyAdapter(getSupportFragmentManager(),this));
+
+        mSlidingTabLayout=(SlidingTabLayout)findViewById(R.id.st1_tabs);
+        mSlidingTabLayout.setDistributeEvenly(true);
+        mSlidingTabLayout.setBackground(getResources().getColorStateList(R.color.colorPrimary));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
