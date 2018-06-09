a9b2396f98ed4d5c87bd5562dc5efb9a8c415875
Bless Ramadewa <bramasuteja@gmail.com>
2018-06-09 10:03:26 +0700

    added layout for elektrik and taman kita

---
+++ app/src/main/java/com/example/faturrahman/rumahkita/features/activity_taman_kita.java
@@ -0,0 +1,13 @@
+package com.example.faturrahman.rumahkita.features;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_taman_kita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taman_kita);
+    }
+}
--- app/src/main/java/com/example/faturrahman/rumahkita/fragment/HistoryFragment.java
+++ app/src/main/java/com/example/faturrahman/rumahkita/fragment/HistoryFragment.java
@@ -20,6 +20,6 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend, container, false);
+        return inflater.inflate(R.layout.fragment_history, container, false);
     }
 }
