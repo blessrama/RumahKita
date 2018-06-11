072dee3e909055e9ac43dd93f32948bc53a78629
Adibta Triantama <adibtatriantama@gmail.com>
2018-06-11 11:13:10 +0700

    fix bug fragment history, taman kita

--- app/src/main/java/com/example/faturrahman/rumahkita/features/activity_taman_kita.java
+++ app/src/main/java/com/example/faturrahman/rumahkita/features/activity_taman_kita.java
@@ -3,6 +3,8 @@ package com.example.faturrahman.rumahkita.features;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.faturrahman.rumahkita.R;
+
 public class activity_taman_kita extends AppCompatActivity {
 
     @Override
