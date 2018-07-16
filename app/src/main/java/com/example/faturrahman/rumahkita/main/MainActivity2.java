49ea8d9016f3743dc5f5e9d62511d619f6513c5d
Bless Ramadewa <bramasuteja@gmail.com>
2018-07-16 20:49:25 +0700

    add spasi

--- app/src/main/java/com/example/faturrahman/rumahkita/main/MainActivity2.java
+++ app/src/main/java/com/example/faturrahman/rumahkita/main/MainActivity2.java
@@ -1,6 +1,6 @@
 package com.example.faturrahman.rumahkita.main;
 
-import android.os.Bundle;
+import  android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
